f295cd8cbfd9d9ddb7138b52cbe306f804da43fb
Ethan Ferguson <ethanf108@gmail.com>
2023-02-20 20:24:41 -0500

    Add seconds

--- src/main.rs
+++ src/main.rs
@@ -45,13 +45,13 @@ fn main() {
         let next_hour_index = hours.iter().position(|&h| h == (hour + 1) % 12).unwrap() as i32;
 
         let hour_theta = (((next_hour_index - hour_index + 12) % 12) as f64
-            * (minute as f64 / 60_f64)
+            * (minute as f64 + (seconds as f64 / 60_f64))
+            / 60_f64
             + hour_index as f64)
             * PI
             / 6_f64;
 
         draw::set_line_style(draw::LineStyle::Solid, 10);
-
         draw::draw_line(
             w / 2,
             h / 2,
@@ -59,13 +59,16 @@ fn main() {
             (hour_theta.sin() * 0.6 * (h / 2) as f64) as i32 + h / 2,
         );
 
-        let minute_index = hours.iter().position(|&h| h == (minute / 5) % 12).unwrap() as i32;
+        let minute_index = hours
+            .iter()
+            .position(|&h| h == (minute / 5 + 11) % 12 + 1)
+            .unwrap() as i32;
         let next_minute_index = hours
             .iter()
-            .position(|&h| h == (minute / 5 + 1) % 12)
+            .position(|&h| h == (minute / 5) % 12 + 1)
             .unwrap() as i32;
 
-        let minute_theta = ((((minute % 5) as f64 / 5_f64)
+        let minute_theta = (((((minute % 5) as f64 + (seconds as f64 / 60_f64)) / 5_f64)
             * ((next_minute_index - minute_index + 12) % 12) as f64)
             + minute_index as f64)
             * PI
@@ -78,5 +81,12 @@ fn main() {
             (minute_theta.sin() * 0.8 * (h / 2) as f64) as i32 + h / 2,
         );
     });
+
+    std::thread::spawn(move || loop {
+        app.redraw();
+        app::awake();
+        std::thread::sleep(std::time::Duration::from_millis(250));
+    });
+
     app.run().unwrap();
 }
