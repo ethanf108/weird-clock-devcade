3a39a80d7b64e828cf001b1260c0070f99479fae
Ethan Ferguson <ethanf108@gmail.com>
2023-03-31 16:51:49 -0400

    Completely revamped project

--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,216 @@
+use bevy::{prelude::*, window::WindowMode};
 use chrono::{Local, Timelike};
-use core::f64::consts::PI;
-use fltk::{prelude::*, *};
-use gilrs::{ev::EventType::ButtonPressed, Event, Gilrs};
-use rand::seq::SliceRandom;
-use rand::thread_rng;
-use std::sync::{Arc, Mutex};
+use core::f32::consts::PI;
+use rand::{seq::SliceRandom, thread_rng};
+
+#[derive(Component)]
+struct Hour {
+    number: u8,
+    index: u8,
+}
+
+#[derive(Component)]
+struct MinuteHand;
+
+#[derive(Component)]
+struct HourHand;
 
 fn main() {
-    let app = app::App::default();
-    let mut window = window::Window::default();
-    window.fullscreen(true);
-    let mut draw = widget::Widget::default();
-    window.end();
-    window.show();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                mode: WindowMode::Fullscreen,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_startup_system(setup)
+        .add_systems(
+            (update_text, update_hour, update_minute).in_schedule(CoreSchedule::FixedUpdate),
+        )
+        .insert_resource(FixedTime::new_from_secs(0.1))
+        .run();
+}
 
-    let mut hours: Vec<u32> = (1..=12).collect();
+fn setup(mut commands: Commands, mut windows: Query<&mut Window>, assets: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    let window_res = &windows.single_mut().resolution;
+    let w = window_res.width() as f32;
+    let h = window_res.height() as f32;
+
+    // Background
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0., 1., 1.),
+            custom_size: Some(Vec2::new(w, h)),
+            ..default()
+        },
+        ..default()
+    });
+
+    // Hours
+
+    let mut hours: Vec<u8> = (1..=12).collect();
     hours.shuffle(&mut thread_rng());
 
-    let arc = Arc::new(Mutex::new(hours));
+    let font = assets.load("OpenSans.ttf");
+    for i in 1..=12 {
+        let text = Text2dBundle {
+            text: Text::from_section(
+                format!("{}", i),
+                TextStyle {
+                    font: font.clone(),
+                    font_size: 50.,
+                    color: Color::BLACK,
+                },
+            ),
+            ..default()
+        };
 
-    let a = arc.clone();
+        commands.spawn((
+            text,
+            Hour {
+                index: *hours.get(i - 1).unwrap(),
+                number: i as u8,
+            },
+        ));
+    }
 
-    draw.draw(move |_| {
-        let hours = a.lock().unwrap();
+    // Hour Hand
 
-        let w = window.w();
-        let h = window.h();
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::BLACK,
+                custom_size: Some(Vec2::new(w / 2. * 0.5, 5.)),
+                ..default()
+            },
+            ..default()
+        },
+        HourHand,
+    ));
 
-        let now = Local::now();
-        let hour = now.hour() % 12;
-        let minute = now.minute();
-        let seconds = now.second();
+    // Minute hand
 
-        draw::draw_rect_fill(0, 0, w, h, enums::Color::Cyan);
-        draw::set_draw_color(enums::Color::Black);
-        draw::set_font(enums::Font::HelveticaBold, (w + h) / 70);
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::BLACK,
+                custom_size: Some(Vec2::new(w / 2. * 0.7, 5.)),
+                ..default()
+            },
+            ..default()
+        },
+        MinuteHand,
+    ));
+}
 
-        for i in 0..=11 {
-            let theta = i as f64 * PI / 6_f64;
-            let num = hours[i as usize];
-            draw::draw_text2(
-                format!("{}", num).as_str(),
-                (theta.cos() * 0.9 * (w / 2) as f64) as i32 + w / 2,
-                (theta.sin() * 0.9 * (h / 2) as f64) as i32 + h / 2,
-                0,
-                0,
-                enums::Align::Center,
-            );
-        }
+fn update_text(windows: Query<&Window>, mut hours: Query<(&mut Transform, &mut Hour)>) {
+    let window_res = &windows.single().resolution;
+    let w = window_res.width() as f32;
+    let h = window_res.height() as f32;
 
-        let hour_index = hours.iter().position(|&h| h == hour).unwrap() as i32;
-        let next_hour_index = hours.iter().position(|&h| h == (hour + 1) % 12).unwrap() as i32;
-
-        let hour_theta = (((next_hour_index - hour_index + 12) % 12) as f64
-            * (minute as f64 + (seconds as f64 / 60_f64))
-            / 60_f64
-            + hour_index as f64)
-            * PI
-            / 6_f64;
-
-        draw::set_line_style(draw::LineStyle::Solid, 10);
-        draw::draw_line(
-            w / 2,
-            h / 2,
-            (hour_theta.cos() * 0.6 * (w / 2) as f64) as i32 + w / 2,
-            (hour_theta.sin() * 0.6 * (h / 2) as f64) as i32 + h / 2,
+    for mut hour in hours.iter_mut() {
+        let transform = &mut hour.0;
+        let theta = hour.1.index as f32 * PI / 6.;
+        transform.translation = Vec3::new(
+            theta.cos() * 0.9 * (w / 2.),
+            theta.sin() * 0.9 * (h / 2.),
+            1.,
         );
+    }
+}
 
-        let minute_index = hours
-            .iter()
-            .position(|&h| h == (minute / 5 + 11) % 12 + 1)
-            .unwrap() as i32;
-        let next_minute_index = hours
-            .iter()
-            .position(|&h| h == (minute / 5) % 12 + 1)
-            .unwrap() as i32;
+fn update_hour(
+    windows: Query<&mut Window>,
+    hours: Query<&Hour>,
+    mut minute_hand: Query<(&mut Transform, With<HourHand>)>,
+) {
+    let window_res = &windows.single().resolution;
+    let w = window_res.width() as f32;
+    let h = window_res.height() as f32;
 
-        let minute_theta = (((((minute % 5) as f64 + (seconds as f64 / 60_f64)) / 5_f64)
-            * ((next_minute_index - minute_index + 12) % 12) as f64)
-            + minute_index as f64)
+    let now = Local::now();
+    let hour = now.hour() % 12;
+    let minute = now.minute();
+    let seconds = now.second();
+
+    let hour_index = hours.iter().find(|h| h.number == hour as u8).unwrap().index;
+    let next_hour_index = hours
+        .iter()
+        .find(|h| h.number == ((hour + 1) % 12) as u8)
+        .unwrap()
+        .index;
+
+    let hour_theta = (((12 + hour_index - next_hour_index) % 12) as f32
+        * (minute as f32 + (seconds as f32 / 60.))
+        / -60.
+        + hour_index as f32)
+        * PI
+        / 6.;
+
+    let transform = &mut minute_hand.single_mut().0;
+
+    transform.rotation =
+        Quat::from_rotation_z((hour_theta.sin() * (h / w)).atan2(hour_theta.cos()));
+    transform.translation = Vec3::new(
+        hour_theta.cos() * 0.5 * w / 4.,
+        hour_theta.sin() * 0.5 * h / 4.,
+        1.,
+    );
+    transform.scale = Vec3::new(
+        hour_theta.sin() * hour_theta.sin() * (h / w) + hour_theta.cos() * hour_theta.cos(),
+        1.,
+        1.,
+    );
+}
+
+fn update_minute(
+    windows: Query<&mut Window>,
+    hours: Query<&Hour>,
+    mut minute_hand: Query<(&mut Transform, With<MinuteHand>)>,
+) {
+    let window_res = &windows.single().resolution;
+    let w = window_res.width() as f32;
+    let h = window_res.height() as f32;
+
+    let now = Local::now();
+    let minute = now.minute();
+    let seconds = now.second();
+    let millis = now.timestamp_millis() % 1000;
+
+    let transform = &mut minute_hand.single_mut().0;
+
+    let minute_index = hours
+        .iter()
+        .find(|&h| h.number == ((minute / 5 + 11) % 12 + 1) as u8)
+        .unwrap()
+        .index;
+    let next_minute_index = hours
+        .iter()
+        .find(|&h| h.number == ((minute / 5) % 12 + 1) as u8)
+        .unwrap()
+        .index;
+
+    let minute_theta =
+        (((((minute % 5) as f32 + ((seconds as f32 + (millis as f32 / 1000.)) / 60.)) / 5.)
+            * ((12 + minute_index - next_minute_index) % 12) as f32)
+            * -1.
+            + minute_index as f32)
             * PI
-            / 6_f64;
+            / 6.;
 
-        draw::draw_line(
-            w / 2,
-            h / 2,
-            (minute_theta.cos() * 0.8 * (w / 2) as f64) as i32 + w / 2,
-            (minute_theta.sin() * 0.8 * (h / 2) as f64) as i32 + h / 2,
-        );
-
-        drop(hours);
-    });
-
-    std::thread::spawn(move || loop {
-        app.redraw();
-        app::awake();
-        std::thread::sleep(std::time::Duration::from_millis(250));
-    });
-
-    let a1 = arc.clone();
-
-    std::thread::spawn(move || {
-        let mut gilrs = Gilrs::new().unwrap();
-        loop {
-            let ev = gilrs.next_event();
-            match ev {
-                Some(Event {
-                    event: ButtonPressed(gilrs::ev::Button::Start, _),
-                    ..
-                }) => {
-                    std::process::exit(0);
-                }
-                Some(Event {
-                    event: ButtonPressed(gilrs::ev::Button::Select, _),
-                    ..
-                }) => {
-                    std::process::exit(0);
-                }
-                _ => {
-                    let hours = &mut a1.lock().unwrap();
-                    hours.shuffle(&mut thread_rng());
-                    drop(hours);
-                    app.redraw();
-                    app::awake();
-                }
-            }
-        }
-    });
-
-    app.run().unwrap();
+    transform.rotation =
+        Quat::from_rotation_z((minute_theta.sin() * (h / w)).atan2(minute_theta.cos()));
+    transform.translation = Vec3::new(
+        minute_theta.cos() * 0.7 * w / 4.,
+        minute_theta.sin() * 0.7 * h / 4.,
+        1.,
+    );
+    transform.scale = Vec3::new(
+        minute_theta.sin() * minute_theta.sin() * (h / w) + minute_theta.cos() * minute_theta.cos(),
+        1.,
+        1.,
+    );
 }
